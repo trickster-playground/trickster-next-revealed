8eba50d22143b9c1fd93decd0dd1a9b1ab68f0e6
trickstercode-dev <trickstercode.dev@gmail.com>
2025-04-30 19:01:08 +0700

    Update Description.tsx

--- components/Description.tsx
+++ components/Description.tsx
@@ -3,10 +3,10 @@ const Description = () => {
     <div className="text-[16.5px] text-white/80 text-justify">
       <p>
         I graduated in{' '}
-        <span className="text-blue-500 font-extrabold">Informatics Engineering</span> from Tanjungpura University and began my coding journey in 2020 while working on my thesis. Since 2022, I've been working professionally as a{' '}
+        <span className="text-blue-500 font-extrabold">Informatics Engineering</span> from Tanjungpura University and began my coding journey in 2020 while working on my thesis. Since 2022, I&apos;ve been working professionally as a{' '}
         <span className="text-blue-500 font-extrabold">Junior Programmer</span>, where I developed a strong passion for{' '}
         <span className="text-blue-500 font-extrabold">website development</span>, particularly with the{' '}
-        <span className="text-blue-500 font-extrabold">Laravel Framework</span>. I'm currently expanding my skill set by exploring other technologies, such as{' '}
+        <span className="text-blue-500 font-extrabold">Laravel Framework</span>. I&apos;m currently expanding my skill set by exploring other technologies, such as{' '}
         <span className="text-blue-500 font-extrabold">Next.js</span>.
       </p>
     </div>
