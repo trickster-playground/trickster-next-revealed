906c6dddaf603f784a95b5ea4f34d8313c7b2f99
trickstercode-dev <trickstercode.dev@gmail.com>
2025-05-02 14:44:29 +0700

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,16 +2,15 @@ import TechStack from './TechStack';
 
 export default function Footer() {
   return (
-    <footer className=''>
-      <div className='mb-8 mt-16 flex items-center justify-between  mx-auto md:mb-10 md:flex md:space-y-0 max-w-4xl text-sm'>
+    <footer className='px-4 mx-4'>
+      <div className='mb-8 mt-16 mx-auto max-w-4xl text-sm flex flex-col justify-center items-center md:flex-row md:items-center md:justify-center gap-4'>
         <TechStack />
 
-        <div className='my-2 flex space-x-2  text-gray-500 dark:text-gray-400'>
-          <div>{`Copyright © ${new Date().getFullYear()}`}</div>
-          <span>{` • `}</span>
-          <span>Andi Muhammad&apos;s - Relentless Learning Journey</span>
+        <div className='text-gray-500 dark:text-gray-400 text-center md:text-end'>
+          <div>{`© ${new Date().getFullYear()} Andi Muhammad – Relentless Learning Journey`}</div>
         </div>
       </div>
     </footer>
+
   );
 }
